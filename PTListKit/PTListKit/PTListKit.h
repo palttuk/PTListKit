d1a2c0653db4263719de13e66fce83f450a5fb8a
palttuk <palttuk40@gmail.com>
2018-01-09 02:39:23 +0900

    프로젝트 생성

---
+++ PTListKit/PTListKit/PTListKit.h
@@ -0,0 +1,19 @@
+//
+//  PTListKit.h
+//  PTListKit
+//
+//  Created by heeso Kim on 2018. 1. 9..
+//  Copyright © 2018년 heeso Kim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PTListKit.
+FOUNDATION_EXPORT double PTListKitVersionNumber;
+
+//! Project version string for PTListKit.
+FOUNDATION_EXPORT const unsigned char PTListKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PTListKit/PublicHeader.h>
+
+
